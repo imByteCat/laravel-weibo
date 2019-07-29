01941581a95f0436c32a126e6dbf1e1612661135
imByteCat <imbytecat@gmail.com>
2019-07-29 23:24:17 +0800

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,6 +13,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'ByteCat';
         $user->email = 'imbytecat@gmail.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
